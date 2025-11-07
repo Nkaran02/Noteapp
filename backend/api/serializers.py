53179d767459837f968b9dbf3edebd51a2b8717f
Sujith N <karan820416@gmail.com>
2025-11-07 16:35:02 +0530

    access and refresh token working

---
+++ backend/api/serializers.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+#sirializer takes json data and convert it to python equalivant code and vice versa take python code and return json data
+#serializer will look at the model and all its fields , it will make sure its valid and pass it 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+        #here we are getting the validated data and creating an user
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+from .serializers import UserSerializer
 
 # Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all() #we are getting all data for creating the new user so we dont create an existing user again 
+    serializer_class = UserSerializer       #this serializer class tells use what kinda data do we need to accept in this case username and password
+    permission_classes = [AllowAny] #this specify who can call this in thiscase anyone
+
